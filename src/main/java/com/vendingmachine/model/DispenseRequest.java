d5a0f44852d0bf629d52d18df352760e9cbe981a
devaanshmann <devaanshmann5@gmail.com>
2025-11-25 11:58:06 -0800

    models: snack and dispense request

--- src/main/java/com/vendingmachine/model/DispenseRequest.java
+++ src/main/java/com/vendingmachine/model/DispenseRequest.java
@@ -1,4 +1,48 @@
 package com.vendingmachine.model;
 
 public class DispenseRequest {
+    private String snackName;
+    private double moneyInserted;
+    private Snack snack;
+    private boolean isValid;
+    private String errorMessage;
+
+    public DispenseRequest(String snackName, double moneyInserted) {
+        this.snackName = snackName;
+        this.moneyInserted = moneyInserted;
+        this.isValid = true;
+        this.errorMessage = "";
+    }
+
+    public String getSnackName() {
+        return snackName;
+    }
+
+    public double getMoneyInserted() {
+        return moneyInserted;
+    }
+
+    public Snack getSnack() {
+        return snack;
+    }
+
+    public void setSnack(Snack snack) {
+        this.snack = snack;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
--- src/main/java/com/vendingmachine/model/Snack.java
+++ src/main/java/com/vendingmachine/model/Snack.java
@@ -1,4 +1,44 @@
 package com.vendingmachine.model;
 
 public class Snack {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Snack(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return quantity > 0;
+    }
+
+    public void decrementQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + String.format("%.2f", price) + ") - Quantity: " + quantity;
+    }
 }
